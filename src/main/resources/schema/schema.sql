f4233a569a5e8eb991c4242b6e48fdfc0223026c
devjun10 <devjun1023@gmail.com>
2024-04-28 08:05:02 +0900

    chore: project build

---
+++ src/main/resources/schema/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users
+(
+    id               BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT 'PK',
+    name             VARCHAR(40)  NOT NULL COMMENT '이름',
+    phone_number     VARCHAR(13)  NOT NULL COMMENT '전화번호',
+    email            VARCHAR(40)  NOT NULL COMMENT 'Email',
+    nickname         VARCHAR(20)  NOT NULL COMMENT '닉네임',
+    password         VARCHAR(255) NOT NULL COMMENT '비밀번호',
+    created_at       TIMESTAMP    NOT NULL COMMENT '생성일',
+    last_modified_at TIMESTAMP    NULL COMMENT '최종 수정일',
+    deleted          BOOLEAN      NOT NULL COMMENT '삭제 유무'
+);
