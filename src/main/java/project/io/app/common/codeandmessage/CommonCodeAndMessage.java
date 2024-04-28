408a640caf408eca078af6cbcdb3571889d4e13c
devjun10 <devjun1023@gmail.com>
2024-04-28 09:47:30 +0900

    feat: 공통 CodeAndMessage 추가 - 전역 컴포넌트에서 사용할 공통 코드/메시지

---
+++ src/main/java/project/io/app/common/codeandmessage/CommonCodeAndMessage.java
@@ -0,0 +1,31 @@
+package project.io.app.common.codeandmessage;
+
+import lombok.*;
+
+@Getter
+public enum CommonCodeAndMessage implements CodeAndMessage {
+    OK(200, "OK"),
+    BAD_REQUEST(400, "Bad Request")
+    ;
+
+    private final int code;
+    private final String message;
+
+    CommonCodeAndMessage(
+        int code,
+        String message
+    ) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public int getCode() {
+        return code;
+    }
+
+    @Override
+    public String getCustomMessage() {
+        return message;
+    }
+}
--- src/main/java/project/io/app/core/user/domain/UserJpaRepository.java
+++ src/main/java/project/io/app/core/user/domain/UserJpaRepository.java
@@ -1,6 +1,11 @@
 package project.io.app.core.user.domain;
 
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.*;
+
+import java.util.*;
 
 public interface UserJpaRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM users u WHERE u.id =:id AND u.deleted =:deleted")
+    Optional<User> findUserByIdAndDeleted(@Param("id") Long id, @Param("deleted") Boolean deleted);
 }
