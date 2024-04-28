5708f7c781e27a4ba72de6645d9f05fe9b79b4ff
devjun10 <devjun1023@gmail.com>
2024-04-28 17:45:56 +0900

    refactor: 201 응답 CodeAndMessage 추가

--- src/main/java/project/io/app/common/codeandmessage/CommonCodeAndMessage.java
+++ src/main/java/project/io/app/common/codeandmessage/CommonCodeAndMessage.java
@@ -5,6 +5,7 @@ import lombok.*;
 @Getter
 public enum CommonCodeAndMessage implements CodeAndMessage {
     OK(200, "OK"),
+    CREATED(201, "Created"),
     BAD_REQUEST(400, "올바른 파라미터를 입력해주세요.");
 
     private final int code;
