e54cdac597a69267ca35f48b8e37d412318bb819
devjun10 <devjun1023@gmail.com>
2024-04-28 10:12:36 +0900

    feat: BadRequest 에러 핸들러 추가 - 사용자 입력 값 Validation 처리를 위한 핸들러 추가

--- src/main/java/project/io/app/common/exception/GlobalExceptionHandler.java
+++ src/main/java/project/io/app/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package project.io.app.common.exception;
 
+import lombok.extern.slf4j.*;
 import org.springframework.http.*;
+import org.springframework.validation.*;
+import org.springframework.web.bind.*;
 import org.springframework.web.bind.annotation.*;
 import project.io.app.common.codeandmessage.*;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -12,5 +16,33 @@ public class GlobalExceptionHandler {
         CodeAndMessage codeAndMessage = exception.getCodeAndMessage();
         return ResponseEntity.status(codeAndMessage.getCode())
             .body(new ErrorResponse(codeAndMessage));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> resolveMethodArgumentNotValidException(
+        MethodArgumentNotValidException exception
+    ) {
+        CodeAndMessage codeAndMessage = CommonCodeAndMessage.BAD_REQUEST;
+        StringBuilder builder = createMessage(exception);
+        log.error("errors:{}", builder);
+        return ResponseEntity.status(codeAndMessage.getCode())
+            .body(new ErrorResponse(codeAndMessage));
+    }
+
+    private StringBuilder createMessage(MethodArgumentNotValidException exception) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("{");
+        BindingResult bindingResult = exception.getBindingResult();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            String field = fieldError.getField();
+            builder.append("[")
+                .append(field)
+                .append("]")
+                .append(" Input:")
+                .append(fieldError.getRejectedValue())
+                .append(" ");
+        }
+        builder.append("}");
+        return builder;
     }
 }
