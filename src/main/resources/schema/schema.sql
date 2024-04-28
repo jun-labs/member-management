f8b0cb1bfd1f3125c1dbd4967bb2cd761dbc3cf4
devjun10 <devjun1023@gmail.com>
2024-04-28 09:22:30 +0900

    chore: userId 칼럼 추가

--- src/main/resources/schema/schema.sql
+++ src/main/resources/schema/schema.sql
@@ -4,6 +4,7 @@ CREATE TABLE users
     name             VARCHAR(40)  NOT NULL COMMENT '이름',
     phone_number     VARCHAR(13)  NOT NULL COMMENT '전화번호',
     email            VARCHAR(40)  NOT NULL COMMENT 'Email',
+    user_id          VARCHAR(40)  NOT NULL COMMENT '사용자 ID',
     nickname         VARCHAR(20)  NOT NULL COMMENT '닉네임',
     password         VARCHAR(255) NOT NULL COMMENT '비밀번호',
     created_at       TIMESTAMP    NOT NULL COMMENT '생성일',
